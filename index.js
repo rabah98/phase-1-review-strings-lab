846c4a70162ba6b217c14b4966506dda98164d06
Rabah <rmerzoug471@gmail.com>
2022-06-25 00:30:49 -0500

    commit tree f74c903ab25df3dac49c135e2d4816f219005acf

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'rabah';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, RABAH!';
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
